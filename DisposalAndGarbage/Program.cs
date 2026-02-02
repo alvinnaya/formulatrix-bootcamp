4092bb6cc93714066b75adeff7f1985c97b7e8a9
ALvian Naya Pradhana <alvinnaya13@gmail.com>
2026-02-02 09:03:45 +0700

    adding DisposalAndGarbage

---
+++ DisposalAndGarbage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+
+
+int x = 10;
+
+Console.WriteLine("hello");
+
+Debug.WriteLine("DEBUG: Program dimulai");
+Trace.WriteLine("TRACE: Program dimulai");
+
+Debug.WriteIf(x > 5, "DEBUG: x lebih besar dari 5");
+Trace.WriteIf(x > 5, "TRACE: x lebih besar dari 5");
+
+
